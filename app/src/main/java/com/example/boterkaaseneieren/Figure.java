cd5518ee2cd6455471a2b579ff18b3b4602c87e7
unknown <christiaangerritsen2000@gmail.com>
2019-04-04 13:47:24 +0200

    Added activity

---
+++ app/src/main/java/com/example/boterkaaseneieren/Figure.java
@@ -0,0 +1,13 @@
+package com.example.boterkaaseneieren;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Figure extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_figure);
+    }
+}
