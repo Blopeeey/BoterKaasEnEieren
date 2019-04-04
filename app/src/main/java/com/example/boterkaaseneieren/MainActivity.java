607601f8cd43db5d6968d7bc0706dd6a048f5a6e
Blopeeey <basweidmann@hotmail.com>
2019-04-04 14:28:01 +0200

    yeet

--- app/src/main/java/com/example/boterkaaseneieren/MainActivity.java
+++ app/src/main/java/com/example/boterkaaseneieren/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.boterkaaseneieren;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void toFigureActivity(View view){
+        Intent figureIntent = new Intent(this, Figure.class);
+        startActivity(figureIntent);
     }
 }
