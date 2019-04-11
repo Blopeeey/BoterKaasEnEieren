42a09d204ed949cd919b538c7eab5c4e054b2d5b
Blopeeey <basweidmann@hotmail.com>
2019-04-11 16:01:54 +0200

    jasperdoetniks

--- app/src/main/java/com/example/boterkaaseneieren/Game.java
+++ app/src/main/java/com/example/boterkaaseneieren/Game.java
@@ -1,7 +1,9 @@
 package com.example.boterkaaseneieren;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class Game extends AppCompatActivity {
@@ -11,5 +13,9 @@ public class Game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game);
+    }
+
+    public void naarNewGame(View view){
+        finish();
     }
 }
