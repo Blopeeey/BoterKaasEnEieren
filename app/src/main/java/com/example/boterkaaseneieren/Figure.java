ab64166e45d61b0abd32961b555f91c02134651a
Blopeeey <basweidmann@hotmail.com>
2019-04-23 12:32:15 +0200

    dropdown

--- app/src/main/java/com/example/boterkaaseneieren/Figure.java
+++ app/src/main/java/com/example/boterkaaseneieren/Figure.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Figure extends AppCompatActivity {
 
@@ -13,6 +15,20 @@ public class Figure extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_figure);
+
+        //Dropdown voor speler 1
+        Spinner speler1 = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Figure.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.speler1));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        speler1.setAdapter(myAdapter);
+
+        //Dropdown voor speler 2
+        Spinner speler2 = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<String> myAdapter2 = new ArrayAdapter<String>(Figure.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.speler2));
+        myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        speler2.setAdapter(myAdapter2);
     }
 
     protected void toGameActivity(View view){
